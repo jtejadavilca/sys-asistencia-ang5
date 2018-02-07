8694d20277cbe76f81e791e42bcf64a93386b030
Jose Tejada <jtejadavilca@gmail.com>
2018-02-06 23:40:34 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule
